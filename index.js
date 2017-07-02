a48844d7516af79b8e70c01bbcba8b4cb13b0f6b
Andrija Petrovic <andrija.hers@gmail.com>
2017-07-02 10:33:37 +0200

    Making use of allex_extractionbufferparser

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function createLib(execlib) {
-  return execlib.loadDependencies('client', ['allex:base:parser'], createVariableLengthTextParser.bind(null, execlib));
+  return execlib.loadDependencies('client', ['allex:base:parser', 'allex:extractionbuffer:parser'], createVariableLengthTextParser.bind(null, execlib));
 }
 
-function createVariableLengthTextParser(execlib, BaseParser) {
+function createVariableLengthTextParser(execlib, BaseParser, DoubleBufferWithCursor) {
   'use strict';
   var lib = execlib.lib,
     q = lib.q,
-    execSuite = execlib.execSuite,
-    parserRegistry = execSuite.parserRegistry;
+    execSuite = execlib.execSuite;
 
   function cutOffByNumber (string, length) {
     return string.substring(length);
@@ -18,167 +17,6 @@ function createVariableLengthTextParser(execlib, BaseParser) {
   function firstChars (string, charnumber) {
     return string.substring(0, charnumber);
   }
-
-  function BufferWithCursor(param) {
-    this.buffer = new Buffer(param);
-    this.cursor = 0;
-    this.anchor = 0;
-  }
-  BufferWithCursor.prototype.destroy = function () {
-    this.anchor = 0;
-    this.cursor = null;
-    this.buffer = null;
-  };
-  BufferWithCursor.prototype.isProcessed = function () {
-    return this.cursor >= this.buffer.length;
-  };
-  BufferWithCursor.prototype.tick = function (howmany) {
-    this.cursor += howmany;
-  };
-  BufferWithCursor.prototype.chunk = function () {
-    return this.buffer.slice(this.anchor, this.cursor);
-  };
-  BufferWithCursor.prototype.chunkLength = function () {
-    if (this.cursor < this.anchor) {
-      throw new lib.Error('INVALID_CURSOR_VS_ANCHOR_POSITION', this.cursor+' cannot be less than '+this.anchor);
-    }
-    return this.cursor - this.anchor;
-  };
-  BufferWithCursor.prototype.tail = function () {
-    return this.buffer.slice(this.anchor);
-  };
-  BufferWithCursor.prototype.resetAnchor = function () {
-    this.anchor = this.cursor;
-  };
-  BufferWithCursor.prototype.remaining = function () {
-    return this.buffer.length - this.anchor;
-  };
-  BufferWithCursor.prototype.unprocessed = function () {
-    return this.buffer.length - this.cursor;
-  };
-  BufferWithCursor.prototype.valueAtCursor = function (offset) {
-    return this.buffer[this.cursor + (this.offset||0)];
-  };
-  BufferWithCursor.prototype.appendFrom = function (other, howmany) {
-    other.buffer.copy(this.buffer,this.cursor,other.cursor,other.cursor+howmany); 
-  };
-  BufferWithCursor.prototype.appendTo = function (other, howmany) {
-    if ('undefined' === typeof howmany) {
-      howmany = this.chunkLength();
-    }
-    this.buffer.copy(other.buffer,other.cursor,this.anchor,this.anchor+howmany);
-    other.cursor+=howmany;
-  };
-  BufferWithCursor.prototype.toString = function () {
-    return this.buffer.slice(this.cursor).toString();
-  };
-
-  function DoubleBufferWithCursor(parser){
-    this.parser = parser;
-    this.current = null;
-    this.previous = null;
-  }
-  DoubleBufferWithCursor.prototype.destroy = function () {
-    if (this.previous) {
-      this.previous.destroy();
-    }
-    this.previous = null;
-    if (this.current) {
-      this.current.destroy();
-    }
-    this.current = null;
-    this.parser = null;
-  };
-  DoubleBufferWithCursor.prototype.purgePrevious = function () {
-    if (!this.previous) {
-      throw new lib.Error('CANNOT_PURGE_PREVIOUS', 'Previous buffer cannot be purged because it does not exist');
-    }
-    this.previous.destroy();
-    this.previous = null;
-  };
-  DoubleBufferWithCursor.prototype.process = function (buffer, cb) {
-    if (this.previous) {
-      if (this.previous.unprocessed() < 1) {
-        this.purgePrevious();
-      } else {
-        throw new lib.Error('CANNOT_SET_BUFFER_PREVIOUS_STILL_EXISTS');
-      }
-    }
-    this.previous = this.current;
-    this.current = new BufferWithCursor(buffer);
-    while (this.current.unprocessed()) {
-      switch (this.atDelimiter()) {
-        case null:
-          return;
-        case true:
-          cb(this.chunk());
-      }
-    }
-    //cb(this.chunk());
-  };
-  DoubleBufferWithCursor.prototype.atDelimiter = function () {
-    var p = this.previous,
-      c = this.current,
-      rd = this.parser.recordDelimiter,
-      dl = rd.length,
-      i = 0,
-      logobj = {dolog: false},
-      w,
-      cunp = c.unprocessed();
-    if (dl > cunp) {
-      if (cunp) {
-        //console.log('that is it,',dl,'>',cunp, 'with tail', c.tail());
-        //console.log('finished with buffer,',c.chunkLength(),'bytes left');
-        return null;
-      } else {
-        return true;
-      }
-    }
-    w = (p && p.unprocessed()) ? p : c;
-    while (i < dl) {
-      if (this.matchesDelimiter(rd, i, w, logobj)) {
-        w = (p && p.unprocessed()) ? p : c;
-      } else {
-        return false;
-      }
-      i++;
-    }
-    return true;
-  };
-  DoubleBufferWithCursor.prototype.matchesDelimiter = function (delimiter, i, buffer, logobj) {
-    if (delimiter[i] !== buffer.valueAtCursor()) {
-      if (logobj.dolog) {
-        //console.log(delimiter[i], '<>', buffer.valueAtCursor(), 'working at', buffer.cursor);
-      }
-      buffer.tick(i ? 1-i : 1); //tricky part - reset cursor to retry matching
-      return false;
-    }
-    logobj.dolog = true;
-    //console.log(delimiter[i], '==', buffer.valueAtCursor(), 'working at', buffer.cursor);
-    buffer.tick(1);
-    return true;
-  };
-  DoubleBufferWithCursor.prototype.chunk = function () {
-    var pr = this.previous ? this.previous.remaining() : 0, l, c, ret;
-    if (pr) {
-      l = pr + this.current.chunkLength();
-      c = new BufferWithCursor(l);
-      this.previous.appendTo(c);
-      //console.log('current',this.current,'will append to chunk',c.buffer);
-      this.current.appendTo(c);
-      //console.log('after appending, chunk',c.buffer);
-      this.previous.destroy();
-      this.previous = null;
-      ret = c.buffer;
-      c.destroy();
-      this.current.resetAnchor();
-      return ret;
-    } else {
-      ret = this.current.chunk();
-      this.current.resetAnchor();
-      return ret;
-    }
-  };
 
   function VariableLengthTextParser(options) {
     BaseParser.call(this, options);
@@ -191,7 +29,6 @@ function createVariableLengthTextParser(execlib, BaseParser) {
     if(!this.fieldList){
       throw new lib.Error('NO_FIELD_LIST','VariableLengthTextParser must have a fieldList Array defined in its prototype');
     }
-    this.buffer = null;
     this.currentlyProcessed = null;
     this.previouslyProcessed = null;
     this.doubleBuffer = new DoubleBufferWithCursor(this);
@@ -210,31 +47,13 @@ function createVariableLengthTextParser(execlib, BaseParser) {
       this.previouslyProcessed.destroy();
     }
     this.previouslyProcessed = null;
-    this.buffer = null;
   };
   VariableLengthTextParser.prototype.fileToData = function (data) {
-    var ret = [];
-    this.doubleBuffer.process(data, this.takeChunk.bind(this, ret));
-    return ret;
-  };
-  VariableLengthTextParser.prototype.takeChunk = function (ret, completechunk) {
-    var rec;
-    if (completechunk) {
-      if (this.isNewRecord(completechunk)) {
-        rec = this.buffer;
-        this.buffer = this.createBuffer(completechunk);
-      } else {
-        this.augmentBuffer(completechunk);
-      }
-      if (rec) {
-        ret.push(this.postProcessFileToData(rec));
-      }
-    }
+    return this.doubleBuffer.process(data);
   };
   VariableLengthTextParser.prototype.finalize = function(){
-    //console.log('finalize',this.doubleBuffer.current.remaining(), this.doubleBuffer.chunk(), 'my buffer', this.buffer);
-    if(this.buffer){
-      return this.postProcessFileToData(this.buffer);
+    if (this.doubleBuffer) {
+      return this.doubleBuffer.finalize();
     }
   };
   VariableLengthTextParser.prototype.isNewRecord = function (data) {
